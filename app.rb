fc2b84669b0c2b4c6a5879667dee65abe0f3c424
KVexcavator <str_token@mail.ru>
2018-09-09 10:33:54 +0600

    all set

--- app.rb
+++ app.rb
@@ -30,20 +30,20 @@ post '/visit' do
 	@color       = params[ :color]
 
 	#хеш сообщений об ошибке
-	hh={ :name       => "Введите имя",
-       :pfone      => "Введите телефон",
-       :data_time  => "Введите дату"
+	hh={ :name       => "Enter name",
+       :pfone      => "Enter pfone",
+       :data_time  => "Enter date and time",
 	}
 
 	hh.each do |key,value|
-	  if params[key]==''
+	  if params[key] == ''
 		  # @eerror переменная об ощибке, нправляетися в вмд
-			@error=hh[key]
-			erb :visit
-		end	
+			@error = hh[key]
+			return erb :visit
+		end
 	end
 	
-	if @name !="" && @pfone!="" && @data_time!=""
+	if @name !="" and @pfone!="" and @data_time!=""
 	  	output=File.open "./public/visit.txt","a"
 	    output.write "Master: #{@master}, Visiter: #{@name} ,Color : #{@color} pfone: #{@pfone}, date & time #{@data_time}<br>"
 	    output.close
